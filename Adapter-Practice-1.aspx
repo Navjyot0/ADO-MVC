ff3714d7168fec56881cc2e2b0f438cd95fcd7dc
Navjyot <38138428+Navjyot0@users.noreply.github.com>
2018-11-28 21:48:40 +0530

    Practice GridView and Adapter

---
+++ Adapter-Practice-1.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Adapter-Practice-1.aspx.cs" Inherits="MvcApplication.Adapter_Practice_1" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:GridView ID="gvEmployee" runat="server" BackColor="White" BorderColor="#E7E7FF"
+            BorderStyle="None" BorderWidth="1px" CellPadding="3" 
+            GridLines="Horizontal" OnRowCancelingEdit="gvEmployee_RowCancelingEdit"
+            OnRowDeleting="gvEmployee_RowDeleting" 
+            OnRowEditing="gvEmployee_RowEditing" OnRowUpdating="gvEmployee_RowUpdating"
+            AllowPaging="true" PageSize="2" 
+            onpageindexchanging="gvEmployee_PageIndexChanging">
+            <AlternatingRowStyle BackColor="#F7F7F7" />
+            <Columns>
+                <asp:CommandField ShowDeleteButton="True" ShowEditButton="True"/>
+            </Columns>
+            <EmptyDataTemplate>
+                No Data Available
+            </EmptyDataTemplate>
+            <FooterStyle BackColor="#B5C7DE" ForeColor="#4A3C8C" />
+            <HeaderStyle BackColor="#4A3C8C" Font-Bold="True" ForeColor="#F7F7F7" />
+            <PagerStyle BackColor="#E7E7FF" ForeColor="#4A3C8C" HorizontalAlign="Right" />
+            <RowStyle BackColor="#E7E7FF" ForeColor="#4A3C8C" />
+            <SelectedRowStyle BackColor="#738A9C" Font-Bold="True" ForeColor="#F7F7F7" />
+            <SortedAscendingCellStyle BackColor="#F4F4FD" />
+            <SortedAscendingHeaderStyle BackColor="#5A4C9D" />
+            <SortedDescendingCellStyle BackColor="#D8D8F0" />
+            <SortedDescendingHeaderStyle BackColor="#3E3277" />
+        </asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Adapter-Practice-1.aspx.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using System.Collections;
+
+namespace MvcApplication
+{
+    public partial class Adapter_Practice_1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                BindGridData();
+            }
+        }
+
+        public void BindGridData()
+        {
+            using (SqlConnection sqlCon = new SqlConnection(ConfigurationManager.ConnectionStrings["conStr"].ConnectionString))
+            {
+                SqlDataAdapter da = new SqlDataAdapter();
+                da.SelectCommand = new SqlCommand();
+                da.SelectCommand.CommandText = "select * from Department; Select * from Employee; ";
+                da.SelectCommand.Connection = sqlCon;
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                //gvEmployee.DataSource = ds.Tables["Table"];
+                //gvEmployee.DataSource = ds.Tables["Table1"];
+                //gvEmployee.DataSource = ds.Tables[0];
+                //gvEmployee.DataSource = ds.Tables[1];
+                ds.Tables[0].TableName = "Department";
+                ds.Tables[1].TableName = "Employee";
+                gvEmployee.DataSource = ds.Tables["Employee"];
+                //gvEmployee.DataSource = ds.Tables["Department"];
+                gvEmployee.DataBind();
+            }
+        }
+
+        protected void gvEmployee_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            gvEmployee.EditIndex = e.NewEditIndex;
+            BindGridData();
+        }
+
+        protected void gvEmployee_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            gvEmployee.EditIndex = -1;
+            BindGridData();
+        }
+
+        protected void gvEmployee_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            using (SqlConnection sqlcon = new SqlConnection(ConfigurationManager.ConnectionStrings["conStr"].ConnectionString))
+            {
+                SqlDataAdapter da = new SqlDataAdapter("select * from employee", sqlcon);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                SqlCommandBuilder scb = new SqlCommandBuilder(da);
+                //da.UpdateCommand = new SqlCommand();
+                //da.UpdateCommand.CommandText = "Update employee firstname='" + e.NewValues["FirstName"] + "', lastname='"+e.NewValues["LastName"]+"', age="+e.NewValues["Age"]+" where Id="+e.NewValues["Id"];
+                ds.Tables[0].PrimaryKey = new DataColumn[] { ds.Tables[0].Columns["Id"] };
+                string FirstName=e.NewValues["FirstName"].ToString();
+                int Id = Convert.ToInt32(e.RowIndex);
+                ds.Tables[0].Rows[Id]["FirstName"] = e.NewValues["FirstName"].ToString();
+                ds.Tables[0].Rows[Id]["LastName"] = e.NewValues["LastName"].ToString();
+                ds.Tables[0].Rows[Id]["Age"] = e.NewValues["Age"].ToString();
+                da.Update(ds);
+                gvEmployee.EditIndex = -1;
+                BindGridData();
+            }
+            BindGridData();
+        }
+
+        protected void gvEmployee_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["conStr"].ConnectionString))
+            {
+                SqlDataAdapter da = new SqlDataAdapter("select * from employee", con);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                SqlCommandBuilder cb = new SqlCommandBuilder(da);
+                ds.Tables[0].PrimaryKey = new DataColumn[] { ds.Tables[0].Columns["Id"] };
+                object obj = e.Values["Id"];
+                (ds).Tables[0].Rows.Find(e.Values["Id"]).Delete();
+                da.Update(ds);
+            }
+            BindGridData();
+        }
+
+        protected void gvEmployee_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            gvEmployee.PageIndex = e.NewPageIndex;
+            BindGridData();
+        }
+    }
+}
